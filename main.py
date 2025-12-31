c582055948ca832194107ea6cb53307cb53e31b1
DevManTowhid <mdtowhidulalam364@gmail.com>
2025-12-31 14:04:08 +0600

    Enhance error message for missing HUGGINGFACEHUB_API_TOKEN and initialize FastAPI app

--- main.py
+++ main.py
@@ -2,7 +2,14 @@
 load_dotenv()
 
 if not os.getenv("HUGGINGFACEHUB_API_TOKEN"):
-    raise ValueError("HUGGINGFACEHUB_API_TOKEN not found in environment variables")
+    raise ValueError(" ❌ CRITICAL: HUGGINGFACEHUB_API_TOKEN not found in environment variables")
+
+
+app = FastAPI(
+    name : "Chat with your PDF",
+    description="An AI-powered PDF assistant that leverages Hugging Face models to answer questions based on the content of your PDF documents.",
+    version="1.0.0",
+)
 
 
 if __name__ == "__main__":
